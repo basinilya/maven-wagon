6998a478a240fe3a97559ff6d5cbbd14c256e86a
Ilya Basin <basinilya@gmail.com>
2018-02-20 17:37:28 +0300

    fix ScmWagon cannot checkout when list() unsupported

--- wagon-providers/wagon-scm/src/main/java/org/apache/maven/wagon/providers/scm/ScmWagon.java
+++ wagon-providers/wagon-scm/src/main/java/org/apache/maven/wagon/providers/scm/ScmWagon.java
@@ -522,7 +522,8 @@ public class ScmWagon
             relPath += p + "/";
 
             File newDir = new File( checkoutDirectory, relPath );
-            if ( !newDir.mkdirs() )
+            newDir.mkdir();
+            if ( !newDir.isDirectory() )
             {
                 throw new TransferFailedException(
                     "Failed to create directory " + newDir.getAbsolutePath() + "; parent should exist: "
